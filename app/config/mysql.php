d2c01b89bf526c0fa5b1a15d06dd1bc245807050
fred <freddev69@gmail.com>
2025-03-05 09:32:17 +0100

    feat(bdd): add mysql connexion on app

---
+++ app/config/mysql.php
@@ -0,0 +1,21 @@
+<?php
+
+try {
+    $dsn = "mysql:host=database;dbname=cours_php;charset=utf8mb4";
+
+    $db = new PDO(
+        $dsn,
+        'root',
+        null,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    ); 
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données: {$error->getMessage()}");
+}
+
+var_dump(
+    $db->query('SELECT * FROM users')->fetchALL()
+);
